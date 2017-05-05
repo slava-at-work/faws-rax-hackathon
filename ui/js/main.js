5bf28d87600bd7cefdb81995e07629b02a95f8a5
Ak <akerdal@60f81dbfc356.ant.amazon.com>
2017-05-05 14:40:55 +1000

    FE finished

---
+++ ui/js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	$(function () {
+        $('#scheduler').datetimepicker({
+            icons: {
+                time: "fa fa-clock-o",
+                date: "fa fa-calendar",
+                up: "fa fa-arrow-up",
+                down: "fa fa-arrow-down",
+                previous: "fa fa-chevron-left",
+                next: "fa fa-chevron-right"
+            }
+        });
+    });
+});
